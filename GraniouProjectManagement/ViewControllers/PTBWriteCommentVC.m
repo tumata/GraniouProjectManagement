355ff4709ec9236d4b481cc9b01d7b32b57c2e0d
Yeti LLC <yetillc@Yetis-MacBook-Pro.local>
2014-07-17 15:30:20 -0700

    little change ui comment

--- GraniouProjectManagement/ViewControllers/PTBWriteCommentVC.m
+++ GraniouProjectManagement/ViewControllers/PTBWriteCommentVC.m
@@ -60,20 +60,10 @@
 // Called when the UIKeyboardDidShowNotification is sent.
 - (void)keyboardWasShown:(NSNotification*)aNotification
 {
-    NSDictionary* info = [aNotification userInfo];
-    CGSize kbSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
-    
-    UIEdgeInsets contentInsets = UIEdgeInsetsMake(0.0, 0.0, kbSize.height, 0.0);
+    UIEdgeInsets contentInsets = UIEdgeInsetsMake(0.0, 0.0, 40.0, 0.0);
     _scrollView.contentInset = contentInsets;
     _scrollView.scrollIndicatorInsets = contentInsets;
-    
-    // If active text field is hidden by keyboard, scroll it so it's visible
-    // Your app might not need or want this behavior.
-    CGRect aRect = self.view.frame;
-    aRect.size.height -= kbSize.height;
-    if (!CGRectContainsPoint(aRect, _textView.frame.origin) ) {
-        [self.scrollView scrollRectToVisible:_textView.frame animated:YES];
-    }
+    [_scrollView setContentOffset:CGPointMake(0, 40) animated:YES];
 }
 
 // Called when the UIKeyboardWillHideNotification is sent

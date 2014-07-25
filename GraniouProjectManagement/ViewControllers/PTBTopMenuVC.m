435660332adf410c0b07151e456d2cb3da81c33b
Philippe Bertin <philippe.t.bertin@gmail.com>
2014-07-25 00:01:21 -0700

    needs better UI menu Nice synchronisation screens pops up on synchronize

--- GraniouProjectManagement/ViewControllers/PTBTopMenuVC.m
+++ GraniouProjectManagement/ViewControllers/PTBTopMenuVC.m
@@ -14,7 +14,7 @@
 
 #import "Tache.h"
 
-@interface PTBTopMenuVC () <PTBNavigationViewDelegate>
+@interface PTBTopMenuVC () <PTBNavigationViewDelegate, UITableViewDelegate, UITableViewDataSource>
 
 @property (weak, nonatomic) IBOutlet PTBNavigationView *navigationView;
 
@@ -36,7 +36,6 @@
 {
     [super viewDidLoad];
     _navigationView.delegate = self;
-    
 }
 
 - (void)didReceiveMemoryWarning
@@ -58,7 +57,7 @@
     NSLog(@"ok");
 }
 
-- (IBAction)listeTachesBoutonPressed:(id)sender {
+- (void)listeTachesBoutonPressed {
     MCIntent* intent = [MCIntent intentWithSectionName:SECTION_MONTEUR andViewName:VIEW_TACHESTABLE];
     [intent setAnimationStyle:UIViewAnimationOptionTransitionCrossDissolve];
     
@@ -69,7 +68,7 @@
 
 
 
-- (IBAction)pressLR:(id)sender {
+- (void)listeLRBoutonPressed {
     MCIntent* intent = [MCIntent intentWithSectionName:SECTION_MONTEUR andViewName:VIEW_TACHESTABLE];
     [intent setAnimationStyle:UIViewAnimationOptionTransitionCrossDissolve];
     
@@ -77,5 +76,57 @@
     
     [[MCViewModel sharedModel] setCurrentSection:intent];
 }
+
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 3;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return  ([[UIScreen mainScreen] bounds].size.height - 64) / 3 ;
+}
+
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"customInfosCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    if (indexPath.row == 0) {
+        cell.textLabel.text = @"Liste des taches";
+    } else if (indexPath.row == 1) {
+        cell.textLabel.text = @"Levees de réserve";
+    } else if (indexPath.row == 2) {
+        cell.textLabel.text = @"Documents";
+    }
+    
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row == 0) {
+        [self listeTachesBoutonPressed];
+    } else if (indexPath.row == 1) {
+        [self listeLRBoutonPressed];
+    } else if (indexPath.row == 2) {
+        NSLog(@"Documents");
+    }
+}
+
 
 @end
